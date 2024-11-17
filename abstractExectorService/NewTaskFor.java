2e8718f0a4533a4b80dda127b4f0f6bea5607e47
avinashsoni9829 <45571070+avinashsoni9829@users.noreply.github.com>
2024-11-18 00:48:24 +0530

    Add files via upload

---
+++ abstractExectorService/NewTaskFor.java
@@ -0,0 +1,10 @@
+package com.example.multithreading.abstractExectorService;
+
+import java.util.concurrent.*;
+
+public class NewTaskFor {
+
+    public static void main(String[] args) {
+
+    }
+}
